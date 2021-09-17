db637651576aa160da889d912023e8c73bcaa9f1
med-amine-fh <fhal.mohamed.amine@gmail.com>
2021-09-17 10:55:48 +0100

    add login with google🔑

--- src/App.js
+++ src/App.js
@@ -1,16 +1,59 @@
 import Post from "./components/blog/blog";
 import Header from "./components/header/header";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { GoogleLogin } from "react-google-login";
+
+const Routing = () => {
+  return (
+    <Switch>
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/blog/:id" component={Blog} />
+      <Route exact path="/*" component={LandingPage} />
+    </Switch>
+  );
+};
+
+const Blog = ({ match }) => {
+  const { id } = match.params;
+
+  return <h1>Blogs N°{id}</h1>;
+};
+
+const Login = () => {
+  const responseGoogle = async (response) => {
+    try {
+      console.log(response);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  return (
+    <div className="mt-5 d-flex flex justify-content-center align-items-center">
+      <GoogleLogin
+        clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+        buttonText="Login with google"
+        onSuccess={responseGoogle}
+      />
+    </div>
+  );
+};
+
+const LandingPage = () => (
+  <>
+    <Header />
+    <div className="posts">
+      <Post />
+      <Post />
+      <Post />
+    </div>
+  </>
+);
 
 function App() {
   return (
-    <>
-      <Header />
-      <div className="posts">
-        <Post />
-        <Post />
-        <Post />
-      </div>
-    </>
+    <Router>
+      <Routing />
+    </Router>
   );
 }
 
--- src/components/blog/blog.js
+++ src/components/blog/blog.js
@@ -1,5 +1,3 @@
-import { Link, Router } from "react-router-dom";
-
 export default function Post() {
   return (
     <div className="card">
